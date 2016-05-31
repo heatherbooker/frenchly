8b5a6280ffad67d7c62d2edf788ca340f5a3c463
Heather Booker <Heather Booker>
2016-05-31 15:51:21 -0700

    setup gulp with browserify , babel, and react

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
-var react = require('gulp-react');
-var reactdom = require('gulp-react-dom');
+// var react = require('gulp-react');
+// var reactdom = require('gulp-react-dom');
+var babel = require('gulp-babel');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
 
 gulp.task('default', ['sass', 'js'], function() {
   console.log('fasd')
@@ -11,6 +14,19 @@ gulp.task('default', ['sass', 'js'], function() {
 gulp.task('watch', function() {
   gulp.watch('src/*.*', ['sass', 'js'])
 })
+
+gulp.task('babel', function() {
+  return gulp.src('src/*.jsx')
+    .pipe(babel())
+    .pipe(gulp.dest('src'));
+});
+
+gulp.task('browserify', function() {
+  return browserify("src/index.js")
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('./build'))
+});
 
 gulp.task('js', function() {
   return gulp.src('src/*.js')
