df7f90b68e07f534d6fc73d66fdd9cca6bce90b0
Heather Booker <Heather Booker>
2016-05-31 17:29:52 -0700

    react badge component up and working in src/index.jsx

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,37 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
-// var react = require('gulp-react');
-// var reactdom = require('gulp-react-dom');
 var babel = require('gulp-babel');
+var watchify = require('watchify');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 
-gulp.task('default', ['sass', 'js'], function() {
-  console.log('fasd')
-});
+gulp.task('default', ['html', 'js'], function() {});
 
 gulp.task('watch', function() {
-  gulp.watch('src/*.*', ['sass', 'js'])
-})
+  gulp.watch('src/*.jsx', ['js'])
+});
 
-gulp.task('babel', function() {
+gulp.task('js', ['bundle'], function() {
   return gulp.src('src/*.jsx')
     .pipe(babel())
     .pipe(gulp.dest('src'));
 });
 
-gulp.task('browserify', function() {
-  return browserify("src/index.js")
+gulp.task('bundle', function() {
+  watchify(browserify("src/index.js"))
     .bundle()
     .pipe(source('bundle.js'))
-    .pipe(gulp.dest('./build'))
+    .pipe(gulp.dest('./build'));
 });
 
-gulp.task('js', function() {
-  return gulp.src('src/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('build'))
+gulp.task('html', function() {
+  gulp.src('src/*.html')
+    .pipe(gulp.dest('build'));
 });
 
 gulp.task('sass', function() {
   return gulp.src('src/*.scss')
+    .pipe(sass())
     .pipe(gulp.dest('build'));
 });
