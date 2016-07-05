eff1739d1380c033cd8c6b8e4622ee211d052c89
Heather Booker <Heather Booker>
2016-07-04 18:24:45 -0700

    change app.listen

--- server.js
+++ server.js
@@ -4,4 +4,6 @@ var path = require('path');
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.listen(8080);
+app.listen(process.env.PORT, '0.0.0.0', function() {
+  console.log('started listening on ', app.url);
+});
